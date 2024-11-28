32afe1272ac79826001e9d4f3ff5dd1e8a944484
jailadin <jailadinshaik@gmail.com>
2024-11-28 16:17:54 +0530

    Customer& Invoice Dbs setup done

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,21 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('welcome', [AuthController::class, 'index'])->name('welcome');
 Route::controller(AuthController::class)->group(function () {
-    Route::get('welcome', 'index')->name('welcome');
+
     Route::post('registerUser',  'register')->name('register');
     Route::post('login', 'login')->name('apilogin');
     Route::get('profile', 'userProfile')->name('profile')->middleware('auth:sanctum');
     Route::get('logout', 'userLogout')->name('apilogout')->middleware('auth:sanctum');
+});
+
+Route::group(['prefix' => 'V1'], function () {
+    Route::apiResource('customers', CustomerController::class); // apiResource like Resource but this avoid the create & edit methods for apis
+    Route::apiResource('invoices', InvoiceController::class);
 });
